2339432656877d4a3c9bc07caff823ea008cd4df
Diogo Xavier <diogoxavierpinto@gmail.com>
2018-09-13 16:20:11 +0100

    Implement preliminary functionality for

--- src/api/response.rs
+++ src/api/response.rs
@@ -1,1 +1,28 @@
-struct Response();
+use serde::{
+    Serialize,
+    Deserialize,
+};
+use serde_json;
+
+use super::events::base_event::BaseEvent;
+use super::actions::base_action::BaseAction;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    event: String,
+    action: String,
+}
+
+impl<'de> Response {
+
+    pub fn new(
+        event: impl BaseEvent + Serialize + Deserialize<'de>,
+        action: impl BaseAction + Serialize + Deserialize<'de>,
+    ) -> Self {
+
+        Response {
+            event: serde_json::to_string(&event).unwrap(),
+            action: serde_json::to_string(&action).unwrap(),
+        }
+    }
+}
